44a88ec96b2f01fe3f6de6e9f451d5e5db97bc11
Benjamin Werner <bewerner23@gmail.com>
2023-10-14 04:18:33 -0400

    fixing config

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -11,10 +11,10 @@ const config = {
   favicon: "img/favicon.ico",
 
   // Set the production url of your site here
-  url: "https://MarkMeHere.com",
+  url: "https://www.markmehere.com",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: "/MarkMeHereDocs/",
+  baseUrl: "/",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
